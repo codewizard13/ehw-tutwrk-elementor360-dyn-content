05161e1cb200af8bc3b074f0c201c537cb80bd7e
Eric L. Hepperle <codewizard13@gmail.com>
2023-02-08 14:53:05 -0600

    DELETE PLUGIN:  Ultimate Dashboard

--- wp-content/plugins/ultimate-dashboard/modules/login-customizer/src/js/preview/listen-changes/label-fields.ts
+++
@@ -1,9 +0,0 @@
-import listenLabelColorFieldChange from "./fields/label/label-color";
-import listenLabelFontSizeFieldChange from "./fields/label/label-font-size";
-
-const listenLabelFieldsChange = () => {
-	listenLabelColorFieldChange();
-	listenLabelFontSizeFieldChange();
-};
-
-export default listenLabelFieldsChange;
